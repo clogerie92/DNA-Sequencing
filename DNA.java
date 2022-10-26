6508232bf008ebd048adee7ec0c905816d04f681
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-26 11:22:20 -0400

    added code to find the start and stop condons and their indecies

--- DNA.java
+++ DNA.java
@@ -12,9 +12,30 @@ public class DNA {
         String dna3 = "ATTAATATGTACTGA";
         // DNA to test
         String dna = dna1;
-        // length of DNA
-        int dnaLength = dna.length();
         // print length of DNA to console
-        System.out.println("This DNA's length is : " + dnaLength);
+        int dnaLength = dna.length();
+        System.out.println("DNA sequence: " + dna);
+        System.out.println();
+        System.out.println("This DNA's length is: " + dnaLength);
+        System.out.println();
+        // print start codon to console
+        String startCodon = dna.substring(0, 3);
+        System.out.println("Start Codon: " + startCodon);
+        System.out.println();
+        // print end codon to console
+        String endCodon = dna.substring(12, 15);
+        System.out.println("End Codon: " + endCodon);
+        System.out.println();
+        // print index of start codon of DNA to console
+        int startCodonIndex = dna.indexOf("ATG");
+        System.out.println("The index of start codon is: " + startCodonIndex);
+        System.out.println();
+        // print index of stop codon of DNA to console
+        int stopCodonIndex = dna.indexOf("TGA");
+        System.out.println("The index of stop codon is: " + stopCodonIndex);
+        System.out.println();
+
+        
+
     }
 }
