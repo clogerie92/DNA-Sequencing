f0914325d72219f3cd9028ad29c04d20c564c20e
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-26 10:43:26 -0400

    added 3 DNA strings and methods to find length and print it to console.

---
+++ DNA.java
@@ -0,0 +1,20 @@
+// DNA class
+public class DNA {
+    // main method
+    public static void main(String[] args) {
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ / 
+        //   / \   \   / \   \  
+        //  ~   `-~ `-`   `-~ `-
+        // DNA strings
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 = "ATTAATATGTACTGA";
+        // DNA to test
+        String dna = dna1;
+        // length of DNA
+        int dnaLength = dna.length();
+        // print length of DNA to console
+        System.out.println("This DNA's length is : " + dnaLength);
+    }
+}
